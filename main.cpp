4a2eb04dd2701907b1f1867024e2fb1ee975511d
hassy <hassy@owl1.logos.ic.i.u-tokyo.ac.jp>
2016-07-13 14:07:37 +0900

    Add sample data for dev.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 int main(int argc, char** argv){
   const std::string src = "./corpus/sample.en";
   const std::string tgt = "./corpus/sample.ja";
+  const std::string srcDev = "./corpus/sample.en.dev";
+  const std::string tgtDev = "./corpus/sample.ja.dev";
 
   Eigen::initParallel();
-  EncDec::demo(src, tgt, src, tgt);
+  EncDec::demo(src, tgt, srcDev, tgtDev);
 
   return 0;
 }
